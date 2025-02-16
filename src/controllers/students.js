dd550a7f42577bc5bd6ff82b45276c78cf0ccc32
sofia11244 <denizsofia03@gmail.com>
2025-02-16 18:14:25 +0300

    .

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -4,15 +4,18 @@ import { getAllStudents, getStudentById } from '../services/students.js';
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 
 export const getStudentsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
+  const filter = parseFilterParams(req.query);
   const students = await getAllStudents({
     page,
     perPage,
     sortBy,
     sortOrder,
+    filter,
   });
 
   const { sortBy, sortOrder } = parseSortParams(req.query);
