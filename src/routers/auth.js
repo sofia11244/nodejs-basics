87dd9ea47a84328c854a708563d0964ce01fb7a7
sofia11244 <denizsofia03@gmail.com>
2025-02-17 17:01:34 +0300

    refresh token

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -8,7 +8,7 @@ import { validateBody } from '../middlewares/validateBody.js';
 import { loginUserSchema} from '../validation/auth.js';
 import { loginUserController } from '../controllers/auth.js';
 import { logoutUserController } from '../controllers/auth.js';
-
+import { refreshUserSessionController } from '../controllers/auth.js';
 
 const router = Router();
 
@@ -27,4 +27,5 @@ router.post(
 
   router.post('/logout', ctrlWrapper(logoutUserController));
 
+  router.post('/refresh', ctrlWrapper(refreshUserSessionController));
 export default router;
